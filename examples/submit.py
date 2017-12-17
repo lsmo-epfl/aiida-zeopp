e192b72460fe5a7cf81922987e4620a092a76592
Leopold Talirz <leopold.talirz@gmail.com>
2017-12-17 22:20:03 +0100

    add rudimentary parser
    For some reason, I still get an error:
    
        self._validate()
      File "/Users/leopold/Applications/miniconda3/envs/aiida_zeopp/lib/python2.7/site-packages/aiida/orm/implementation/general/calculation/job/__init__.py", line 134, in _validate
        "parser.".format(self.get_parser_name())
    aiida.common.exceptions.ValidationError: No valid class/implementation found for the parser 'zeopp.network'. Set the parser to None if you do not need an automatic parser.
    
    no idea why... I can without problems
    from aiida_zeopp.parsers.network import NetworkParser

--- examples/submit.py
+++ examples/submit.py
@@ -8,29 +8,23 @@ Note: This script assumes you have set up computer and code as in README.md.
 import os
 
 # use code name specified using 'verdi code setup'
-code = Code.get_from_string('zeopp')
-
-# use computer name specified using 'verdi computer setup'
-computer = Computer.get('localhost')
+code = Code.get_from_string('zeopp@localhost')
 
 # Prepare input parameters
 NetworkParameters = DataFactory('zeopp.parameters')
 parameters = NetworkParameters({'cssr': True})
 SinglefileData = DataFactory('singlefile')
-inputfile = SinglefileData(file=os.path.abspath('HKUST-1.cif'))
+structure = SinglefileData(file=os.path.abspath('HKUST-1.cif'))
 
 # set up calculation
 calc = code.new_calc()
 calc.label = "aiida_zeopp format conversion"
 calc.description = "Test converting .cif to .cssr format"
 calc.set_max_wallclock_seconds(1 * 60)
-# This line is only needed for local codes, otherwise the computer is
-# automatically set from the code
-calc.set_computer(computer)
 calc.set_withmpi(False)
 calc.set_resources({"num_machines": 1})
 calc.use_parameters(parameters)
-calc.use_input_structure(inputfile)
+calc.use_input_structure(structure)
 
 calc.store_all()
 calc.submit()
