42bf7e09be94fe8cd70baae02066348e8242d0fd
Leopold Talirz <leopold.talirz@gmail.com>
2018-06-06 23:17:51 +0200

    prepare 0.1.1 release
     * update test script
     * update description of pypi

---
+++ manage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import unittest
+import sys
+from aiida.utils.capturing import Capturing
+from aiida.utils.fixtures import fixture_manager, _PYTEST_FIXTURE_MANAGER
+from aiida_zeopp.tests import get_backend
+
+tests = unittest.defaultTestLoader.discover('.')
+runner = unittest.TextTestRunner()
+
+_PYTEST_FIXTURE_MANAGER.backend = get_backend()
+with Capturing():
+    with fixture_manager() as manager:
+        result = runner.run(tests)
+
+exit_code = int(not result.wasSuccessful())
+sys.exit(exit_code)
