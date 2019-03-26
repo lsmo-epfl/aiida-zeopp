733bcf1c11b970f0e51bd49c99ad59621d378eba
Leopold Talirz <leopold.talirz@gmail.com>
2019-03-26 23:52:54 +0100

    add singularity

--- aiida_zeopp/__init__.py
+++ aiida_zeopp/__init__.py
@@ -3,4 +3,10 @@ AiiDA Zeo++ Plugin
 
 """
 
+from __future__ import absolute_import
 __version__ = "0.2.0"
+
+# disable psycopg2 warning
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module='psycopg2')
+warnings.filterwarnings("ignore", category=UserWarning, module='pymatgen')
