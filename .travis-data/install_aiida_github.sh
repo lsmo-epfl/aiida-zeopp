39b49c2c52066cdcad4863e6c3d52e925955760b
Leopold Talirz <leopold.talirz@gmail.com>
2019-04-04 14:47:59 +0200

    fix aiida-core github install

--- .travis-data/install_aiida_github.sh
+++ .travis-data/install_aiida_github.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git clone https://github.com/aiidateam/aiida_core
-cd aiida_core
+# don't want aiida-core to mess with folders in the repo
+git clone https://github.com/aiidateam/aiida_core ..
+cd ../aiida_core
 git checkout $AIIDA_DEVELOP_GIT_HASH 
 pip install -e .[docs,pre-commit,testing]
-cd ..
+cd -
