e5f748ab3942affebc395ac3d14c282fec6d2f3e
Pezhman Zarabadi-Poor <pezhman.zarabadi@ceitec.muni.cz>
2019-07-18 17:22:09 +0200

    Bug fix on block file validation (#28)
    File handle might have been in wrong position.

--- aiida_zeopp/parsers/network.py
+++ aiida_zeopp/parsers/network.py
@@ -61,22 +61,22 @@ class NetworkParser(Parser):
 
             with self.retrieved.open(fname, 'rb') as handle:
                 if parser is None:
-
+                    
                     # just add file, if no parser implemented
                     parsed = SinglefileData(file=handle)
                     self.out(link, parsed)
-
+                    
                     # workaround: if block pocket file is empty, raise an error
                     # (it indicates the calculation did not finish)
                     if link == 'block':
-                        content = handle.read()
-
-                        if not content.strip():
+                        if not parsed.get_content().strip():
                             self.logger.error(
                                 "Empty block file. This indicates the calculation of blocked pockets did not finish."
                             )
                             empty_block = True
 
+
+                            
                 else:
                     # else parse and add keys to output_parameters
                     try:
